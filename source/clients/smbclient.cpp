7d08405220a26c5ea120a5e2286fa425b4252e8b
Chee Yee <chee.k.yee@boeing.com>
2026-07-20 14:21:18 -0700

    update smbclient to use async functions

--- source/clients/smbclient.cpp
+++ source/clients/smbclient.cpp
@@ -145,52 +145,207 @@ int SmbClient::Get(const std::string &outputfile, const std::string &ppath, uint
 }
 
 
+struct AsyncOpenContext
+{
+    struct smb2fh *fh;
+    int result;
+    bool complete;
+};
+
+static void smb2_async_open_cb(struct smb2_context *smb2, int status, void *command_data, void *private_data)
+{
+    AsyncOpenContext *ctx = (AsyncOpenContext *)private_data;
+    if (status < 0)
+    {
+        ctx->fh = NULL;
+        ctx->result = 0;
+    }
+    else
+    {
+        ctx->fh = (struct smb2fh *)command_data;
+        ctx->result = 1;
+    }
+    ctx->complete = true;
+}
+
+static void smb2_async_close_cb(struct smb2_context *smb2, int status, void *command_data, void *private_data)
+{
+    bool *complete = (bool *)private_data;
+    *complete = true;
+}
+
 int SmbClient::GetRange(const std::string &ppath, DataSink &sink, uint64_t size, uint64_t offset)
 {
-	std::string path = std::string(ppath);
-	path = Util::Trim(path, "/");
-	struct smb2fh *in = smb2_open(smb2, path.c_str(), O_RDONLY);
-	if (in == NULL)
-	{
-		return 0;
-	}
+    std::string path = std::string(ppath);
+    path = Util::Trim(path, "/");
 
-	int ret = this->GetRange((void *)in, sink, size, offset);
-	smb2_close(smb2, in);
+    // Async open
+    AsyncOpenContext open_ctx = {};
+    open_ctx.fh = NULL;
+    open_ctx.result = 0;
+    open_ctx.complete = false;
 
-	return ret;
+    int ret = smb2_open_async(smb2, path.c_str(), O_RDONLY, smb2_async_open_cb, &open_ctx);
+    if (ret != 0)
+        return 0;
+
+    struct pollfd pfd;
+    while (!open_ctx.complete)
+    {
+        pfd.fd = smb2_get_fd(smb2);
+        pfd.events = smb2_which_events(smb2);
+        if (poll(&pfd, 1, 1000) < 0)
+            return 0;
+        if (pfd.revents == 0)
+            continue;
+        if (smb2_service(smb2, pfd.revents) < 0)
+            return 0;
+    }
+
+    if (open_ctx.fh == NULL)
+        return 0;
+
+
+    // Async read
+    int result = this->GetRange((void *)open_ctx.fh, sink, size, offset);
+
+    // Async close
+    bool close_complete = false;
+    smb2_close_async(smb2, open_ctx.fh, smb2_async_close_cb, &close_complete);
+    while (!close_complete)
+    {
+        pfd.fd = smb2_get_fd(smb2);
+        pfd.events = smb2_which_events(smb2);
+        if (poll(&pfd, 1, 1000) < 0)
+            break;
+        if (pfd.revents == 0)
+            continue;
+        if (smb2_service(smb2, pfd.revents) < 0)
+            break;
+    }
+
+    return result;
+}
+
+struct AsyncReadContext
+{
+    DataSink *sink;
+    uint8_t *buff;
+    size_t bytes_remaining;
+    int result;
+    bool complete;
+    struct smb2_context *smb2;
+    struct smb2fh *fh;
+    uint32_t max_read_size;
+};
+
+static void smb2_async_read_cb(struct smb2_context *smb2, int status, void *command_data, void *private_data)
+{
+    AsyncReadContext *ctx = (AsyncReadContext *)private_data;
+
+    if (status < 0)
+    {
+        ctx->result = 0;
+        ctx->complete = true;
+        return;
+    }
+
+    if (status == 0)
+    {
+        ctx->complete = true;
+        return;
+    }
+
+    bool ok = ctx->sink->write((char *)ctx->buff, status);
+    if (!ok)
+    {
+        ctx->result = 0;
+        ctx->complete = true;
+        return;
+    }
+
+    ctx->bytes_remaining -= status;
+    if (ctx->bytes_remaining == 0)
+    {
+        ctx->complete = true;
+        return;
+    }
+
+    size_t bytes_to_read = std::min<size_t>(ctx->max_read_size, ctx->bytes_remaining);
+    int ret = smb2_pread_async(ctx->smb2, ctx->fh, ctx->buff, bytes_to_read, 0, smb2_async_read_cb, ctx);
+    if (ret != 0)
+    {
+        ctx->result = 0;
+        ctx->complete = true;
+    }
+}
+
+static void smb2_async_lseek_cb(struct smb2_context *smb2, int status, void *command_data, void *private_data)
+{
+    AsyncReadContext *ctx = (AsyncReadContext *)private_data;
+
+    if (status < 0)
+    {
+        ctx->result = 0;
+        ctx->complete = true;
+        return;
+    }
+
+    size_t bytes_to_read = std::min<size_t>(ctx->max_read_size, ctx->bytes_remaining);
+    int ret = smb2_pread_async(ctx->smb2, ctx->fh, ctx->buff, bytes_to_read, 0, smb2_async_read_cb, ctx);
+    if (ret != 0)
+    {
+        ctx->result = 0;
+        ctx->complete = true;
+    }
 }
 
 int SmbClient::GetRange(void *fp, DataSink &sink, uint64_t size, uint64_t offset)
 {
-	struct smb2fh *in = (struct smb2fh *)fp;
+    struct smb2fh *in = (struct smb2fh *)fp;
 
-	smb2_lseek(smb2, in, offset, SEEK_SET, NULL);
+    uint8_t *buff = (uint8_t *)malloc(max_read_size);
+    if (buff == NULL)
+        return 0;
 
-	uint8_t *buff = (uint8_t *)malloc(max_read_size);
-	int count = 0;
-	size_t bytes_remaining = size;
-	do
-	{
-		size_t bytes_to_read = std::min<size_t>(max_read_size, bytes_remaining);
-		count = smb2_read(smb2, in, buff, bytes_to_read);
-		if (count > 0)
-		{
-			bytes_remaining -= count;
-			bool ok = sink.write((char *)buff, count);
-			if (!ok)
-			{
-				free((uint8_t *)buff);
-				return 0;
-			}
-		}
-		else
-		{
-			break;
-		}
-	} while (1);
+    AsyncReadContext ctx = {};
+    ctx.sink = &sink;
+    ctx.buff = buff;
+    ctx.bytes_remaining = size;
+    ctx.result = 1;
+    ctx.complete = false;
+    ctx.smb2 = smb2;
+    ctx.fh = in;
+    ctx.max_read_size = max_read_size;
 
-	free((char *)buff);
+    // Kick off async lseek, which chains into async reads
+    int ret = smb2_lseek_async(smb2, in, offset, SEEK_SET, smb2_async_lseek_cb, &ctx);
+    if (ret != 0)
+    {
+        free(buff);
+        return 0;
+    }
 
-	return 1;
+    // Service the event loop until transfer is complete
+    struct pollfd pfd;
+    while (!ctx.complete)
+    {
+        pfd.fd = smb2_get_fd(smb2);
+        pfd.events = smb2_which_events(smb2);
+        if (poll(&pfd, 1, 1000) < 0)
+        {
+            ctx.result = 0;
+            break;
+        }
+        if (pfd.revents == 0)
+            continue;
+        if (smb2_service(smb2, pfd.revents) < 0)
+        {
+            ctx.result = 0;
+            break;
+        }
+    }
+
+    free(buff);
+    return ctx.result;
 }
